e4df38ee470a4616141d32be669320a4dbdce4d6
tortu93 <eltorturo93@gmail.com>
2022-03-18 18:02:13 -0700

    dashboard fix Nota model

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\NotaController;
+use App\Models\Nota;
 
 /*
 |--------------------------------------------------------------------------
